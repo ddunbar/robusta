3db341be7089a44b83d7fd6217cad9d127ce2d37
Daniel Dunbar <daniel@zuster.org>
2011-06-07 16:02:38 -0700

    Add some minimal logic to log users in.

--- robusta/ui/views.py
+++ robusta/ui/views.py
@@ -1,5 +1,10 @@
 import flask
+from flask import current_app
+from flask import redirect
 from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
 
 ###
 # Top-level Information
@@ -9,3 +14,18 @@ frontend = flask.Module(__name__)
 @frontend.route('/')
 def index():
     return render_template("index.html")
+
+###
+# Session Management
+
+@frontend.route('/login', methods=['POST'])
+def login():
+    # Log the user in.
+    username = request.form['username']
+    session['active_user'] = username
+    return redirect(url_for("index"))
+
+@frontend.route('/logout')
+def logout():
+    session.pop('active_user', None)
+    return redirect(url_for("index"))
